1923d24acffd8058ff52006864fb98e2ec700ff0
mandeer <mandeer163@163.com>
2018-04-29 16:39:28 +0800

    clean code

--- models/Faster_RCNN.py
+++ models/Faster_RCNN.py
@@ -1,22 +1,41 @@
 from __future__ import division
-import torch as t
 import numpy as np
 import cupy as cp
-from utils import array_tool as at
-from models.utils.bbox_tools import loc2bbox
 from models.utils.nms import non_maximum_suppression
-
 from torch import nn
-from data.dataset import preprocess
-from torch.nn import functional as F
-from utils.config import opt
-
 
 class FasterRCNN(nn.Module):
+    """Base class for Faster R-CNN.
+
+        This is a base class for Faster R-CNN links supporting object detection
+        API [#]_. The following three stages constitute Faster R-CNN.
+
+        1. **Feature extraction**: Images are taken and their \
+            feature maps are calculated.
+        2. **Region Proposal Networks**: Given the feature maps calculated in \
+            the previous stage, produce set of RoIs around objects.
+        3. **Localization and Classification Heads**: Using feature maps that \
+            belong to the proposed RoIs, classify the categories of the objects \
+            in the RoIs and improve localizations.
+
+        Args:
+            extractor (nn.Module): A module that takes a BCHW image
+                array and returns feature maps.
+            rpn (nn.Module): A module that has the same interface as
+                :class:`model.region_proposal_network.RegionProposalNetwork`.
+                Please refer to the documentation found there.
+            head (nn.Module): A module that takes
+                a BCHW variable, RoIs and batch indices for RoIs. This returns class
+                dependent localization paramters and class scores.
+            loc_normalize_mean (tuple of four floats): Mean values of
+                localization estimates.
+            loc_normalize_std (tupler of four floats): Standard deviation
+                of localization estimates.
+
+        """
     def __init__(self, extractor, rpn, head,
                 loc_normalize_mean = (0., 0., 0., 0.),
-                loc_normalize_std = (0.1, 0.1, 0.2, 0.2)
-    ):
+                loc_normalize_std = (0.1, 0.1, 0.2, 0.2)):
         super(FasterRCNN, self).__init__()
         self.extractor = extractor
         self.rpn = rpn
@@ -33,6 +52,24 @@ class FasterRCNN(nn.Module):
         return self.head.n_class
 
     def forward(self, x, scale=1.):
+        """Forward Faster R-CNN.
+            Args:
+                x (autograd.Variable): 4D image variable.
+                scale (float): the threshold used to select small object
+
+            Returns:
+                Variable, Variable, array, array:
+                Returns tuple of four values listed below.
+
+                * **roi_cls_locs**: Offsets and scalings for the proposed RoIs. \
+                        Its shape is :math:`(R', (L + 1) \\times 4)`.
+                * **roi_scores**: Class predictions for the proposed RoIs. \
+                        Its shape is :math:`(R', L + 1)`.
+                * **rois**: RoIs proposed by RPN. Its shape is \
+                        :math:`(R', 4)`.
+                * **roi_indices**: Batch indices of RoIs. Its shape is \
+                        :math:`(R',)`.
+        """
         img_size = x.shape[2:]
 
         feat = self.extractor(x)
@@ -72,113 +109,6 @@ class FasterRCNN(nn.Module):
         score = np.concatenate(score, axis=0).astype(np.float32)
         return bbox, label, score
 
-    def predict(self, imgs,sizes=None,visualize=False):
-        """Detect objects from images.
-
-        This method predicts objects for each image.
-
-        Args:
-            imgs (iterable of numpy.ndarray): Arrays holding images.
-                All images are in CHW and RGB format
-                and the range of their value is :math:`[0, 255]`.
-
-        Returns:
-           tuple of lists:
-           This method returns a tuple of three lists,
-           :obj:`(bboxes, labels, scores)`.
-
-           * **bboxes**: A list of float arrays of shape :math:`(R, 4)`, \
-               where :math:`R` is the number of bounding boxes in a image. \
-               Each bouding box is organized by \
-               :math:`(y_{min}, x_{min}, y_{max}, x_{max})` \
-               in the second axis.
-           * **labels** : A list of integer arrays of shape :math:`(R,)`. \
-               Each value indicates the class of the bounding box. \
-               Values are in range :math:`[0, L - 1]`, where :math:`L` is the \
-               number of the foreground classes.
-           * **scores** : A list of float arrays of shape :math:`(R,)`. \
-               Each value indicates how confident the prediction is.
-
-        """
-        self.eval()
-        if visualize:
-            self.use_preset('visualize')
-            prepared_imgs = list()
-            sizes = list()
-            for img in imgs:
-                size = img.shape[1:]
-                img = preprocess(at.tonumpy(img))
-                prepared_imgs.append(img)
-                sizes.append(size)
-        else:
-             prepared_imgs = imgs 
-        bboxes = list()
-        labels = list()
-        scores = list()
-        for img, size in zip(prepared_imgs, sizes):
-            img = t.autograd.Variable(at.totensor(img).float()[None], volatile=True)
-            scale = img.shape[3] / size[1]
-            roi_cls_loc, roi_scores, rois, _ = self(img, scale=scale)
-            # We are assuming that batch size is 1.
-            roi_score = roi_scores.data
-            roi_cls_loc = roi_cls_loc.data
-            roi = at.totensor(rois) / scale
-
-            # Convert predictions to bounding boxes in image coordinates.
-            # Bounding boxes are scaled to the scale of the input images.
-            mean = t.Tensor(self.loc_normalize_mean).cuda(). \
-                repeat(self.n_class)[None]
-            std = t.Tensor(self.loc_normalize_std).cuda(). \
-                repeat(self.n_class)[None]
-
-            roi_cls_loc = (roi_cls_loc * std + mean)
-            roi_cls_loc = roi_cls_loc.view(-1, self.n_class, 4)
-            roi = roi.view(-1, 1, 4).expand_as(roi_cls_loc)
-            cls_bbox = loc2bbox(at.tonumpy(roi).reshape((-1, 4)),
-                                at.tonumpy(roi_cls_loc).reshape((-1, 4)))
-            cls_bbox = at.totensor(cls_bbox)
-            cls_bbox = cls_bbox.view(-1, self.n_class * 4)
-            # clip bounding box
-            cls_bbox[:, 0::2] = (cls_bbox[:, 0::2]).clamp(min=0, max=size[0])
-            cls_bbox[:, 1::2] = (cls_bbox[:, 1::2]).clamp(min=0, max=size[1])
-
-            prob = at.tonumpy(F.softmax(at.tovariable(roi_score), dim=1))
-
-            raw_cls_bbox = at.tonumpy(cls_bbox)
-            raw_prob = at.tonumpy(prob)
-
-            bbox, label, score = self._suppress(raw_cls_bbox, raw_prob)
-            bboxes.append(bbox)
-            labels.append(label)
-            scores.append(score)
-
-        self.use_preset('evaluate')
-        self.train()
-        return bboxes, labels, scores
-
-    def get_optimizer(self):
-        """
-        return optimizer, It could be overwriten if you want to specify 
-        special optimizer
-        """
-        lr = opt.lr
-        params = []
-        for key, value in dict(self.named_parameters()).items():
-            if value.requires_grad:
-                if 'bias' in key:
-                    params += [{'params': [value], 'lr': lr * 2, 'weight_decay': 0}]
-                else:
-                    params += [{'params': [value], 'lr': lr, 'weight_decay': opt.weight_decay}]
-        if opt.use_adam:
-            self.optimizer = t.optim.Adam(params)
-        else:
-            self.optimizer = t.optim.SGD(params, momentum=0.9)
-        return self.optimizer
-
-    def scale_lr(self, decay=0.1):
-        for param_group in self.optimizer.param_groups:
-            param_group['lr'] *= decay
-        return self.optimizer
 
 
 
