86921520f501b1e5fec68834098feba1c447693e
mandeer <mandeer163@163.com>
2018-06-07 00:47:59 +0800

    del fasterRCNN code

--- models/Faster/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from .Faster_RCNN_VGG import FasterRCNNVGG16
-from .Faster_RCNN import fast_rcnn_loc_loss
--- models/Faster/utils/nms/__init__.py
+++
@@ -1,1 +0,0 @@
-from .non_maximum_suppression import non_maximum_suppression
