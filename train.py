289896f56cbf9b207861b9647c2801ca3761b633
mandeer <mandeer163@163.com>
2018-06-25 23:52:33 +0800

    clean code

--- train.py
+++ train.py
@@ -9,10 +9,79 @@ from dataloader import get_data_loader
 from models import RetinaNet
 from models.loss import FocalLoss
 
+class Solver(object):
+    def __init__(self, config, model, trainLoader, testLoader):
+        self.model = model
+        self.trainLoader = trainLoader
+        self.testLoader = testLoader
+        self.n_classes = config.n_classes
+        self.use_cuda = config.use_cuda
+
+        self.optimizer = optim.SGD(self.model.parameters(), lr=config.lr, momentum=0.9, weight_decay=1e-4)
+        self.criterion = FocalLoss(num_classes=self.n_classes)
+        if self.use_cuda:
+            self.model = self.model.cuda()
+            self.criterion = self.criterion.cuda()
+
+        self.n_epochs = config.n_epochs
+        self.log_step = config.log_step
+        self.out_path = config.out_path
+
+    def train(self, epoch):
+        print('\nEpoch: %d' % epoch)
+        self.model.train()
+        train_loss = 0
+        for batch_idx, (inputs, loc_targets, cls_targets) in enumerate(self.trainLoader):
+            inputs = inputs.to(device)
+            loc_targets = loc_targets.to(device)
+            cls_targets = cls_targets.to(device)
+
+            self.optimizer.zero_grad()
+            loc_preds, cls_preds = self.model(inputs)
+            loss = self.criterion(loc_preds, loc_targets, cls_preds, cls_targets)
+            loss.backward()
+            self.optimizer.step()
+
+            train_loss += loss.item()
+            print('train_loss: %.3f | avg_loss: %.3f [%d/%d]'
+                  % (loss.item(), train_loss / (batch_idx + 1), batch_idx + 1, len(self.trainLoader)))
+
+    def test(self, epoch):
+        print('\nTest')
+        self.model.eval()
+        test_loss = 0
+        with torch.no_grad():
+            for batch_idx, (inputs, loc_targets, cls_targets) in enumerate(self.testLoader):
+                inputs = inputs.to(device)
+                loc_targets = loc_targets.to(device)
+                cls_targets = cls_targets.to(device)
+
+                loc_preds, cls_preds = self.model(inputs)
+                loss = self.criterion(loc_preds, loc_targets, cls_preds, cls_targets)
+                test_loss += loss.item()
+                print('test_loss: %.3f | avg_loss: %.3f [%d/%d]'
+                      % (loss.item(), test_loss / (batch_idx + 1), batch_idx + 1, len(self.testLoader)))
+
+        # Save checkpoint
+        global best_loss
+        test_loss /= len(self.testLoader)
+        if test_loss < best_loss:
+            print('Saving..')
+            state = {
+                'net': self.model.state_dict(),
+                'loss': test_loss,
+                'epoch': epoch,
+            }
+            if not os.path.isdir(os.path.dirname(args.checkpoint)):
+                os.mkdir(os.path.dirname(args.checkpoint))
+            torch.save(state, args.checkpoint)
+            best_loss = test_loss
+
 def main(config):
     # use cuda ?
     if config.use_cuda:
         from torch.backends import cudnn
+        cudnn.benchmark = True
     elif torch.cuda.is_available():
         print("WARNING: You have a CUDA device, so you should probably run with --cuda")
 
@@ -34,71 +103,10 @@ def main(config):
     print('train samples num: ', len(trainLoader), '  test samples num: ', len(testLoader))
 
     # net
-    device = 'cuda' if config.use_cuda else 'cpu'
-    net = RetinaNet(num_classes=config.n_classes).to(device)
-    print(net)
-    if device == 'cuda':
-        net = torch.nn.DataParallel(net)
-        cudnn.benchmark = True
+    model = RetinaNet(num_classes=config.n_classes)
+    print(model)
 
-    criterion = FocalLoss(num_classes=config.n_classes)
-    optimizer = optim.SGD(net.parameters(), lr=config.lr, momentum=0.9, weight_decay=0.0001)
-
-    # Training
-    def train(epoch):
-        print('\nEpoch: %d' % epoch)
-        net.train()
-        train_loss = 0
-        for batch_idx, (inputs, loc_targets, cls_targets) in enumerate(trainLoader):
-            inputs = inputs.to(device)
-            loc_targets = loc_targets.to(device)
-            cls_targets = cls_targets.to(device)
-
-            optimizer.zero_grad()
-            loc_preds, cls_preds = net(inputs)
-            loss = criterion(loc_preds, loc_targets, cls_preds, cls_targets)
-            loss.backward()
-            optimizer.step()
-
-            train_loss += loss.item()
-            print('train_loss: %.3f | avg_loss: %.3f [%d/%d]'
-                  % (loss.item(), train_loss / (batch_idx + 1), batch_idx + 1, len(trainLoader)))
-
-    def test(epoch):
-        print('\nTest')
-        net.eval()
-        test_loss = 0
-        with torch.no_grad():
-            for batch_idx, (inputs, loc_targets, cls_targets) in enumerate(testLoader):
-                inputs = inputs.to(device)
-                loc_targets = loc_targets.to(device)
-                cls_targets = cls_targets.to(device)
-
-                loc_preds, cls_preds = net(inputs)
-                loss = criterion(loc_preds, loc_targets, cls_preds, cls_targets)
-                test_loss += loss.item()
-                print('test_loss: %.3f | avg_loss: %.3f [%d/%d]'
-                      % (loss.item(), test_loss / (batch_idx + 1), batch_idx + 1, len(testLoader)))
-
-        # Save checkpoint
-        global best_loss
-        test_loss /= len(testLoader)
-        if test_loss < best_loss:
-            print('Saving..')
-            state = {
-                'net': net.state_dict(),
-                'loss': test_loss,
-                'epoch': epoch,
-            }
-            if not os.path.isdir(os.path.dirname(args.checkpoint)):
-                os.mkdir(os.path.dirname(args.checkpoint))
-            torch.save(state, args.checkpoint)
-            best_loss = test_loss
-
-    start_epoch = 0
-    for epoch in range(start_epoch, start_epoch + 200):
-        train(epoch)
-        test(epoch)
+    solver = Solver(config, model, trainLoader, testLoader)
 
 
 if __name__ == '__main__':
